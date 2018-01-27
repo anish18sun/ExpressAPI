3841c7ebd0a9170f982ea9b81836beac93f25636
anish.singh <anish.singh@dailyhunt.in>
2018-01-27 11:11:08 +0530

    ElasticSearch Api almost done

--- app.js
+++ app.js
@@ -4,67 +4,70 @@ var fs = require('fs');
 var multer = require('multer');
 var express = require('express');
 var bodyParser = require('body-parser');
-var mongoClient = require('mongodb').MongoClient;
+var elasticsearch = require('elasticsearch');
 
-var contentdb;
 var app = express();
 var upload = multer({dest: 'Uploads/'});
+
+var elasticClient = new elasticsearch.Client({
+	host: 'localhost:9200'
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : true}));
 
-// connect to the database; the database(and collection) must exist(pre-created)
-
-// mongoClient.connect('mongodb://localhost:27017/ugcontent', function(err, db) {
-// 	if(err) throw err;
-// 	contentdb = db;
-// });
-
-/*			INSTRUCTIONS 
- * To hit the api from your localhost, go the project directory in terminal and
- * type 'node app.js' - this will start the node server. Now to make get request
- * to the base url(index.html), in browser type 'localhost:8000/', similarly
- * for other types of requests 'localhost:8000/images'
-*/
-
-//the methods to handle incoming requests
+// the methods to handle incoming requests
 
 app.get('/', function(req, res) {
 	res.sendFile('/home/anish/Documents/ExpressAPI/index.html');
 });
 
-// the index.html(ajax) will hit this url-path with tag request
 app.get('/images', function(req, res) {
-	var images = [];
-	var queryTags = req.body.tags.split(",");
-
-	// uncomment the lines below to load any image files from the filesystem, can be used for testing
-	// load any file from your filesystem, change the directory path
-	// var img1 = fs.readFileSync("/home/anish/Pictures/gatesian.jpg");
-	// var img2 = fs.readFileSync("/home/anish/Pictures/warrenwork.jpg");
-
-	console.log(queryTags);
-
+	var queryTags = req.query.tags;
+	console.log('queryTags: ' + queryTags);
 	res.writeHead(200, {'Content-Type': 'image/jpg'});
-	res.write(img1);	// send the 1st image
-	res.write(img2);	// send the 2nd image
-	res.end();
+
+	elasticClient.search({
+		index: 'imageindex',
+		body: {
+			query: {
+				match: {
+					tags: queryTags
+				}
+			}
+		}
+	}, function(error, response) {
+		console.log('error: ' + error);
+
+		if(response.hits) {
+			for(let doc of response.hits.hits) {
+				let imgId = doc._id;
+				let imgFile = fs.readFileSync("/home/anish/Documents/ExpressAPI/Uploads/" + imgId);
+				res.write(imgFile);
+			}
+		}
+		res.end();
+	});
 });
 
 app.post('/upload', upload.single('photo'), function(req, res) {
-	// contentdb.collection('ImageData').insertOne({
-	// 	fileName : req.file.filename,
-	// 	originalName : req.file.originalName,
-	// 	tags : req.body.tags.split(",")
-	// });
-	
-	// only multipart/form-data type requests are allowed
+	elasticClient.index({
+		index: 'imageindex',
+		type: 'imagedata',
+		id: req.file.filename,
+		body: {
+			name: req.file.originalname,
+			tags: req.body.tags
+		}
+	}, function(error, response) {
+		if(error) {
+			console.log('error:' + error);
+		}
+	});
 
 	console.log(req.file);
 	console.log(req.body);
 	res.send('image saved');
 });
-
-// when the server closes use contentdb.close();
 
 app.listen(8000);
