a6a5157ab29d3cd6cf8d9e8f48cf58d14ae6764f
anish.singh <anish.singh@dailyhunt.in>
2018-01-31 15:32:30 +0530

    changes incorporated, code restructured, features complete

--- app.js
+++ app.js
@@ -13,8 +13,18 @@ app.use(bodyParser.urlencoded({extended : true}));
 app.use('/assets', express.static('assets'));
 app.use('/imageassets', express.static('Uploads'));
 
-const upload = multer({dest: 'Uploads/'});
-const queryUpload = multer({dest: 'ImageSearch/queries'});
+const ROOT_DIR = '/home/anish/Documents/ExpressAPI/';
+
+const storage = multer.diskStorage({
+	destination: ROOT_DIR + 'Uploads',
+	filename: function(req, file, cb) {
+		const dotIndex = file.originalname.lastIndexOf('.');
+		const ext = file.originalname.substring(dotIndex);
+		cb(null, Date.now() + ext);
+	}
+});
+
+const upload = multer({ storage: storage });
 
 const elasticClient = new elasticsearch.Client({
 	host: 'localhost:9200'
@@ -24,7 +34,7 @@ const gvisionClient = new vision.ImageAnnotatorClient();
 // the methods to handle incoming requests
 
 app.get('/', function(req, res) {
-	res.sendFile('/home/anish/Documents/ExpressAPI/index.html');
+	res.sendFile(ROOT_DIR + 'index.html');
 });
 
 app.get('/images', function(req, res) {
@@ -36,7 +46,11 @@ app.get('/images', function(req, res) {
 		body: {
 			query: {
 				match: {
-					tags: queryTags
+					tags: {
+						query: queryTags,
+						analyzer: 'english',
+						fuzziness: 'AUTO'
+					}
 				}
 			}
 		}
@@ -53,103 +67,24 @@ app.get('/images', function(req, res) {
 });
 
 app.get('/more', function(req, res) {
-	const queryTags = req.query.tags;
-	console.log('queryTags:' + queryTags);
-	
-	elasticClient.search({
-		index: 'imageindex',
-		body: {
-			query: {
-				more_like_this : {
-					fields : ["tags"],
-					like: queryTags
-				}
-			}
-		}
-	}, function(error, response) {
-		console.log('error: ' + error);
-		const imgSrcArr = [];
-		if(response.hits.hits.length > 0) {
-			for(let doc of response.hits.hits) {
-				imgSrcArr.push(doc._id);
-			}
-			res.send(imgSrcArr.join());
-		} else {
-			// please put image file 'nosugg.jpg' in Uploads folder
-			res.send("nosugg.jpg");		
-		}
-	});
-});
-
-app.post('/imageQuery', queryUpload.single('queryPhoto'), function(req, res) {
-
-	const fileName = req.file.filename;
+	const fileId = req.query.fileid;
 	cmd.get(`
 		cd ImageSearch
-		python search.py --index index.csv --query queries/${fileName} --result-path ../Uploads
+		python search.py --index index.csv --query ../Uploads/${fileid} --result-path ../Uploads
 		`,
 		function(err, data, stderr) {
-			console.log('The error in image search: ' + err);
-			console.log('The data from image search: ' + data);
-
-			var srcArr = data.split(",");
-
-			res.send(`
-				<html>
-					<h3>Server sent hi! ${srcArr[0]}</h3>
-					<img src="http://localhost:8000/imageassets/${srcArr[0]}" />
-					<img src="http://localhost:8000/imageassets/${srcArr[1]}" />
-					<img src="http://localhost:8000/imageassets/${srcArr[2]}" />
-					<img src="http://localhost:8000/imageassets/${srcArr[3]}" />
-				</html>
-			`);
-	});
+			res.send(data);
+		});
 });
 
 app.post('/upload', upload.single('photo'), function(req, res) {
-
-	const fileName = req.file.filename;
-	cmd.get(`
-		cd ImageSearch
-		python index.py --dataset ../Uploads --index index.csv
-		`,
-	 	function(err, data, stderr) {
-	 		console.log('The error in image indexing : ' + err);
-	 		console.log('The data from image indexing : ' + data);
-	 });
-
-	elasticClient.index({
-		index: 'imageindex',
-		type: 'imagedata',
-		id: req.file.filename,
-		body: {
-			name: req.file.originalname,
-			tags: req.body.tags
-		}
-	}, function(error, response) {
-		if(error) {
-			console.log('error:' + error);
-		}
-	});
-
-	console.log(req.file);
-	console.log(req.body);
 	res.send('image saved');
 });
 
-app.post('/uploadTagged', upload.single('photo'), function(req, res) {
-	cmd.get(`
-		cd ImageSearch
-		python index.py --dataset ../Uploads --index index.csv
-		`,
-	 	function(err, data, stderr) {
-	 		console.log('The error in image indexing : ' + err);
-	 		console.log('The data from image indexing : ' + data);
-	 });
-
+app.post('/uploadGif', upload.single('photo'), function(req, res) {
 	const gTagsArr = [];
 	const fileName = req.file.filename;
-	gvisionClient.labelDetection("/home/anish/Documents/ExpressAPI/Uploads/" + fileName)
+	gvisionClient.labelDetection(ROOT_DIR + "Uploads/" + fileName)
 		.then(results => {
 			const labels = results[0].labelAnnotations;
 			labels.forEach(label => gTagsArr.push(label.description));
@@ -166,16 +101,56 @@ app.post('/uploadTagged', upload.single('photo'), function(req, res) {
 				if(error) {
 					console.log('error:' + error);
 				}
+				console.log(req.file);
+				console.log(req.body);
 				console.log('image tags(ES scope): ' + gTagsArr.join(","));
+
+				res.send('gif saved');
 			});
 		})
 		.catch(err => {
 			console.log('error: ' + err);
 		});
+});
 
-	console.log(req.file);
-	console.log(req.body);
-	res.send('image saved');
+app.post('/uploadImg', upload.single('photo'), function(req, res) {
+	cmd.get(`
+		cd ImageSearch
+		python index.py --dataset ../Uploads --index index.csv
+		`,
+	 	function(err, data, stderr) {
+	 		console.log('The error in image indexing : ' + err);
+	 });
+
+	const gTagsArr = [];
+	const fileName = req.file.filename;
+	gvisionClient.labelDetection(ROOT_DIR + "/Uploads/" + fileName)
+		.then(results => {
+			const labels = results[0].labelAnnotations;
+			labels.forEach(label => gTagsArr.push(label.description));
+
+			elasticClient.index({
+				index: 'imageindex',
+				type: 'imagedata',
+				id: fileName,
+				body: {
+					name: req.file.originalname,
+					tags: req.body.tags + " " + gTagsArr.join(" ")
+				}
+			}, function(error, response) {
+				if(error) {
+					console.log('error:' + error);
+				}
+				console.log(req.file);
+				console.log(req.body);
+				console.log('image tags(ES scope): ' + gTagsArr.join(","));
+
+				res.send('image saved');
+			});
+		})
+		.catch(err => {
+			console.log('error: ' + err);
+		});
 });
 
 app.listen(8000);
